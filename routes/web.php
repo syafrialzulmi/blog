16df9f17b38ee631b791007644d5ce9714002dbb
Syafrial Zulmi <syafrialzulmi@gmail.com>
2020-09-25 06:18:06 +0700

    aktif state

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,21 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function() {
     return view('home');
 });
 
-Route::view('contact', 'contact');
 Route::view('about', 'about');
+Route::view('contact', 'contact');
 Route::view('login', 'login');
+
+// Route::get('about', function() {
+// Route::get('about', function(Request $request) {
+    // return $request->fullUrl(); //http://127.0.0.1:8000/about
+    // request()->fullUrl()
+
+    // return $request->path();    //about
+    // request()->path()
+    // return request()->is('about') ? true : false;
+    // return view('home');
+// });
