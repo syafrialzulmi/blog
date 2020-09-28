8d4f278e09987b7168e6448dcdb23109675bd0df
Syafrial Zulmi <syafrialzulmi@gmail.com>
2020-09-28 13:29:06 +0700

    Eloquent make model

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -9,7 +10,13 @@ class PostController extends Controller
     public function show($slug) 
     {
         // return $slug;
-        $post = \DB::table('posts')->where('slug', $slug)->first();
+        // $post = \App\Post::where('slug', $slug)->firstOrFail();
+        $post = Post::where('slug', $slug)->firstOrFail();
+
+        // if (is_null($post)) {
+        // if (!($post)) {
+        //     abort(404);
+        // }
 
         return view('posts.show', compact('post'));
     }
