0a4499d1ed51c08d198fd1f7a0060e8100289d07
Syafrial Zulmi <syafrialzulmi@gmail.com>
2020-09-28 07:26:44 +0700

    route wildcard

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+@section('title', $slug)
+@section('content')
+    <div class="container">
+        <p>{{ $slug }}</p>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'HomeController');
 
-// Route::get('/', 'HomeController@index');
-
-// Route::get('/', function() {
-//     $name = request('name');
-//     return view('home', ['name' => $name]);
-// });
+Route::get('posts/{slug}', 'PostController@show');
 
 Route::view('about', 'about');
 Route::view('contact', 'contact');
