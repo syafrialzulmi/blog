5da35202f8081a97f47f5f38e49cdf6bd1da98c7
Syafrial Zulmi <syafrialzulmi@gmail.com>
2020-09-29 15:15:04 +0700

    tinker

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     // protected $table = 'post';
+    public function scopelatestFirst()
+    {
+        return $this->latest()->first();
+    }
 }
