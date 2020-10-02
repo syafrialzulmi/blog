368a034f8c11ccd4ee44c705526e52d772904cea
Syafrial Zulmi <syafrialzulmi@gmail.com>
2020-10-02 15:32:59 +0700

    many to many

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,10 +1,14 @@
-@extends('layouts.app')
-@section('title', $post->title)
+@extends('layouts.app', ['title' => 'Post detail'])
 @section('content')
     <div class="container">
         <h1>{{ $post->title }}</h1>
         <div class="text-secondary">
-            <a href="/categories/{{ $post->category->slug }}">{{ $post->category->title }}</a> &middot; {{ $post->created_at->format('d F, Y') }}
+            <a href="/categories/{{ $post->category->slug }}">{{ $post->category->title }}</a> 
+            &middot; {{ $post->created_at->format('d F, Y') }}
+            &middot; 
+            @foreach ($post->tags as $tag)
+                <a href="#">{{ $tag->title }}</a>
+            @endforeach
         </div>
         <hr>
         <p>{{ $post->body }}</p>
